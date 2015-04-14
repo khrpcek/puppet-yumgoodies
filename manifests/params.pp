c6598bdb09f147c929bd6402ea83f203a3e90028
Kevin Hrpcek <kevin.hrpcek@qg.com>
2015-04-14 08:44:34 -0500

    added strace and tcpdump

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,9 @@
 class yumgoodies::params {
-  $goodies = ['screen', 'htop', 'vim-enhanced', 'iotop', 'lsscsi', 'xinetd', 'sysstat', 'expect', 'nfs-utils', 'sendmail', 'sendmail-cf', 'ksh', 'lsof', 'bash-completion', 'ncdu', 'mtr']
-  $el5_goodies = ['yum--downloadonly', 'qg-nrpe']
-  $el6_goodies = ['yum-plugin-downloadonly', 'qg-nrpe', 'cifs-utils', 'lrzip', 'pigz', 'pxz']
+  $goodies = ['screen', 'htop', 'vim-enhanced', 'iotop', 'lsscsi', 'sysstat', 'expect', 'nfs-utils', 'sendmail', 'sendmail-cf', 'ksh', 'lsof', 'bash-completion', 'ncdu', 'mtr', 'rsync', 'strace', 'tcpdump']
+  $el5_goodies = ['yum--downloadonly' ]
+  $el6_goodies = ['yum-plugin-downloadonly', 'cifs-utils', 'lrzip', 'pigz', 'pxz']
   # waiting for pxz to show up for EL7...
-  $el7_goodies = ['bind-utils', 'net-tools', 'nrpe', 'cifs-utils', 'lrzip', 'pigz']
+  $el7_goodies = ['bind-utils', 'net-tools', 'cifs-utils', 'lrzip', 'pigz']
   $goodies_extras = []
   $el5_goodies_extras = []
   $el6_goodies_extras = []
