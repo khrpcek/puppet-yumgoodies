345bf0d434cd34bcbbc32f8387658f202898c0c0
Kevin Hrpcek <kevin.hrpcek@qg.com>
2015-06-18 13:18:30 -0500

    added ncurses

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,9 +1,9 @@
 class yumgoodies::params {
-  $goodies = ['screen', 'htop', 'vim-enhanced', 'iotop', 'lsscsi', 'sysstat', 'expect', 'nfs-utils', 'sendmail', 'sendmail-cf', 'ksh', 'lsof', 'bash-completion', 'ncdu', 'mtr', 'rsync', 'strace', 'tcpdump', 'mailx', 'ncurses-term']
+  $goodies = ['screen', 'htop', 'vim-enhanced', 'iotop', 'lsscsi', 'sysstat', 'expect', 'nfs-utils', 'sendmail', 'sendmail-cf', 'ksh', 'lsof', 'bash-completion', 'ncdu', 'mtr', 'rsync', 'strace', 'tcpdump', 'mailx', 'ncurses']
   $el5_goodies = ['yum--downloadonly' ]
-  $el6_goodies = ['yum-plugin-downloadonly', 'cifs-utils', 'lrzip', 'pigz', 'pxz']
+  $el6_goodies = ['yum-plugin-downloadonly', 'cifs-utils', 'lrzip', 'pigz', 'pxz', 'ncurses-term']
   # waiting for pxz to show up for EL7...
-  $el7_goodies = ['bind-utils', 'net-tools', 'cifs-utils', 'lrzip', 'pigz']
+  $el7_goodies = ['bind-utils', 'net-tools', 'cifs-utils', 'lrzip', 'pigz', 'ncurses-term']
   $goodies_extras = []
   $el5_goodies_extras = []
   $el6_goodies_extras = []
