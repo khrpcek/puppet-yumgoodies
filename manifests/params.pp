dec8a3c4fd84402284247c2b8498633489ff90f2
Kevin Hrpcek <kevin.hrpcek@qg.com>
2015-06-18 11:20:45 -0500

    added mailx and ncurses-term

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class yumgoodies::params {
-  $goodies = ['screen', 'htop', 'vim-enhanced', 'iotop', 'lsscsi', 'sysstat', 'expect', 'nfs-utils', 'sendmail', 'sendmail-cf', 'ksh', 'lsof', 'bash-completion', 'ncdu', 'mtr', 'rsync', 'strace', 'tcpdump']
+  $goodies = ['screen', 'htop', 'vim-enhanced', 'iotop', 'lsscsi', 'sysstat', 'expect', 'nfs-utils', 'sendmail', 'sendmail-cf', 'ksh', 'lsof', 'bash-completion', 'ncdu', 'mtr', 'rsync', 'strace', 'tcpdump', 'mailx', 'ncurses-term']
   $el5_goodies = ['yum--downloadonly' ]
   $el6_goodies = ['yum-plugin-downloadonly', 'cifs-utils', 'lrzip', 'pigz', 'pxz']
   # waiting for pxz to show up for EL7...
