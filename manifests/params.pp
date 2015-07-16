2b94018e60b7c174b29b503c40f599ce0719ac29
Kevin Hrpcek <khrpcek@gmail.com>
2015-07-15 22:06:44 -0500

    better readme

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class yumgoodies::params {
-  $goodies = ['screen', 'htop', 'vim-enhanced', 'iotop', 'lsscsi', 'sysstat', 'expect', 'nfs-utils', 'sendmail', 'sendmail-cf', 'ksh', 'lsof', 'bash-completion', 'ncdu', 'mtr', 'rsync', 'strace', 'tcpdump', 'mailx', 'ncurses', 'strace', 'tcpdump']
+  $goodies = ['screen', 'htop', 'vim-enhanced', 'iotop', 'lsscsi', 'sysstat', 'expect', 'nfs-utils', 'sendmail', 'sendmail-cf', 'ksh', 'lsof', 'bash-completion', 'ncdu', 'mtr', 'rsync', 'strace', 'tcpdump', 'mailx', 'ncurses' ]
   $el5_goodies = ['yum--downloadonly' ]
   $el6_goodies = ['yum-plugin-downloadonly', 'cifs-utils', 'lrzip', 'pigz', 'pxz', 'ncurses-term']
   # waiting for pxz to show up for EL7...
